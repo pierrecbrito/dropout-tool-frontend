aa1dd8a0c23485c84a26bd792aeb8896531698f1
Pierre Brito <carlospierre07@gmail.com>
2019-07-21 17:48:51 -0300

    Conserto da dependência JQuery

--- js/login.js
+++ js/login.js
@@ -1,16 +1,19 @@
-//Mostra o ícone conforme a necessidade de qual ícone ocular mostar.
-$('#olho').on('click', evento => {
-    if($('#campo_senha').attr('type') === 'password') {
-        $('#campo_senha').prop('type', 'text')
+$(function() {
+    //Mostra o ícone conforme a necessidade de qual ícone ocular mostar.
+    $('#olho').on('click', evento => {
+        if($('#campo_senha').attr('type') === 'password') {
+            $('#campo_senha').prop('type', 'text')
 
-        $('#icone-olho').removeClass('fa-eye')
-        $('#icone-olho').addClass('fa-eye-slash')
-    } else {
-        $('#campo_senha').prop('type', 'password')
+            $('#icone-olho').removeClass('fa-eye')
+            $('#icone-olho').addClass('fa-eye-slash')
+        } else {
+            $('#campo_senha').prop('type', 'password')
 
-        $('#icone-olho').removeClass('fa-eye-slash')
-        $('#icone-olho').addClass('fa-eye')
-    }  
+            $('#icone-olho').removeClass('fa-eye-slash')
+            $('#icone-olho').addClass('fa-eye')
+        }  
+    })
+
 })
 
-$('#visual-container').slideUp(1000)
+
