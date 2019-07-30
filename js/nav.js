f71d460e9a8e517d35af31478ad4ca54055cb3cf
Pierre Brito <carlospierre07@gmail.com>
2019-07-30 14:11:43 -0300

    Ajustes na tela de perfil e no mapa dela

--- js/nav.js
+++ js/nav.js
@@ -1,11 +1,13 @@
-toggle()
+toggle(0.01)
+$('#lista-relatorio').slideToggle()
 
-function toggle(e = null) {
+
+function toggle(speed, e = null) {
     $('#content').toggleClass('content-without-sidebar')
-    $('#sidebar').toggle(250)
+    $('#sidebar').toggle(speed)
 }
 
-$(".fa-bars").click(toggle)
+$(".fa-bars").click(e => toggle(250, e))
 
 $('#relatorios').click(e => {
     $('#lista-relatorio').slideToggle()
