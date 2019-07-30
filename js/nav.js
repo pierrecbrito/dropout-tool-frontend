5088d4fdf97e76da95d82d402da7c03c559a600d
Pierre Brito <carlospierre07@gmail.com>
2019-07-30 12:44:41 -0300

    Implementação d eitem dropdown no menu

--- js/nav.js
+++ js/nav.js
@@ -1,4 +1,3 @@
-
 $(".fa-bars").click(function(event) {
     //$('.nav-esquerda').toggleClass("w-100")
     //$('.nav-esquerda').toggleClass("w-0")
@@ -10,15 +9,5 @@ $(".fa-bars").click(function(event) {
 
 $('#relatorios').click(e => {
     $('#lista-relatorio').slideToggle()
-})
-
-$(".fa-search").click(function(event) {
-    if($('#input-search').css('display') == 'none') {
-        $('.nav-procura').fadeToggle(500)
-    } else {
-        $('#formulario-busca').submit()
-    }
-
-    event.preventDefault()
 })
 
